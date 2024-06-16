813df83d99fe1591ce04c3955610a1ebcfad7e8b
Wnikiel <wojtek@nikiel.org>
2024-06-16 19:44:57 +0200

    Added reading json file

--- text_samples/SamplesReader.py
+++ text_samples/SamplesReader.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import logging
+import json
 
 
 class SamplesReader:
@@ -19,3 +20,17 @@ class SamplesReader:
         if not all_texts:
             logging.warning("No text in passed location: %s", self._files_path_location)
         return all_texts
+
+    def read_json_file(self) -> dict[str, str]:
+        all_json_files = self._files_path_location.glob("*.json")
+
+        all_text = {}
+
+        for json_file in all_json_files:
+            with json_file.open('r', encoding='utf-8') as file:
+                readed_json = json.load(file)
+                all_text[json_file.name] = readed_json
+
+        if not all_text:
+            logging.warning("No text in passed location: %s", self._files_path_location)
+        return all_text
