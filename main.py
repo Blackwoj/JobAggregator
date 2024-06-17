c2d789fab62d09a907abdaf1d8b74ff0a4265bf9
Wnikiel <wojtek@nikiel.org>
2024-06-17 17:53:45 +0200

    Fix reader and add hashing for site layout

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from Llama3.Llama3Model import Llama3Model
 from text_samples.SamplesReader import SamplesReader
 import hashlib
 from database.MongoDB_MAnager import DbManager
+from logging import getLogger
+logger = getLogger(__name__),
 
 def calculate_hash(text):
     hasher = hashlib.sha256()
@@ -13,15 +15,17 @@ def calculate_hash(text):
 base_folder_path = Path(__file__).resolve().parent
 sample_text_folder = base_folder_path / "Samples"
 
-
 if __name__ == "__main__":
-    text_classification_model = Llama3Model()
+    # text_classification_model = Llama3Model()
     text_reader = SamplesReader()
-    db_manager = DbManager()
-
     sample_text = text_reader.read_json_file()
-
+    db_manager = DbManager()
+    i = 1
     for text in sample_text:
-        if db_manager.check_duplicate(text, ["job_offer_url"])
-        text["job_offer_url"]
-        text["cleaned_html"]
+        if db_manager.check_duplicate(text, ["job_offer_url"]):
+            offer_hash = calculate_hash(text["cleaned_html"])
+            if db_manager.check_hash(text, offer_hash):
+                continue
+        # model_output = text_classification_model.classify_offer(text["cleaned_html"])
+        # print(model_output)
+
--- text_samples/SamplesReader.py
+++ text_samples/SamplesReader.py
@@ -5,10 +5,10 @@ import json
 
 class SamplesReader:
 
-    def __init__(self, files_path_location: Path = Path(__file__).resolve().parent / "samples"):
+    def __init__(self, files_path_location: Path = Path(__file__).resolve().parent.parent / "samples"):
         self._files_path_location = files_path_location
 
-    def read_txt_files(self) -> dict[str, str]:
+    def read_files(self) -> dict[str, str]:
         all_txt_files = self._files_path_location.glob('*.txt')
 
         all_texts = {}  # Lista przechowująca zawartość wszystkich plików tekstowych
@@ -23,14 +23,14 @@ class SamplesReader:
 
     def read_json_file(self) -> list[dict[str, str]]:
         all_json_files = self._files_path_location.glob("*.json")
-
-        all_text = {}
+        all_text = []
 
         for json_file in all_json_files:
             with json_file.open('r', encoding='utf-8') as file:
-                readed_json = json.load(file)
-                all_text[json_file.name] = readed_json
-
+                reader_json = json.load(file)
+                for i in range(len(reader_json)):
+                    if reader_json[i]["career_site"] != "not found":
+                        all_text.append(reader_json[i])
         if not all_text:
             logging.warning("No text in passed location: %s", self._files_path_location)
         return all_text
